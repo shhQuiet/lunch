c0bf674e30e05fd2a74502f8a926d79393aa8497
shhQuiet <stevehorne@gmail.com>
2013-12-29 14:05:55 -0600

    cross-origin headers

--- routes/places.js
+++ routes/places.js
@@ -11,8 +11,10 @@ function getCollection(ctx) {
 
 exports.getPlaces = function(ctx, req, res) {
     getCollection(ctx).find().toArray(function(err, result) {
+        var ref = req.get('Referer');
         result.forEach(ctx.convertToExternal);
-        res.jsonp({
+        res.set('Access-Control-Allow-Origin', ref);
+        res.send({
             places: result
         });
     });
